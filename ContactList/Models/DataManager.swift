b0c214b53bb7a6e9ee295edf0480a26688f123f8
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2021-09-07 21:44:56 +0300

    Setup project

---
+++ ContactList/Models/DataManager.swift
@@ -0,0 +1,10 @@
+//
+//  DataManager.swift
+//  ContactList
+//
+//  Created by Олег Федоров on 07.09.2021.
+//
+
+class DataManager {
+    
+}
---
+++ ContactList/Models/Person.swift
@@ -0,0 +1,10 @@
+//
+//  Person.swift
+//  ContactList
+//
+//  Created by Олег Федоров on 07.09.2021.
+//
+
+struct Person {
+    
+}
