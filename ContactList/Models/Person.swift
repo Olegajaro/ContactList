35047e48ff1ae551ea91a5a2487059150d01cd10
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2021-09-07 22:53:35 +0300

    Created models Person and DataManager (version 1)

--- ContactList/Models/Person.swift
+++ ContactList/Models/Person.swift
@@ -6,5 +6,28 @@
 //
 
 struct Person {
+    var name:String
+    var surname: String
+    var phone: String
+    var email: String
     
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+    
+    static func getPerson() -> [Person] {
+        var persons: [Person] = []
+        
+        for _ in 1...9 {
+            let person = Person(
+                name: DataManager.data().names.randomElement() ?? "",
+                surname: DataManager.data().surnames.randomElement() ?? "",
+                phone: DataManager.data().phones.randomElement() ?? "",
+                email: DataManager.data().emails.randomElement() ?? ""
+            )
+            persons.append(person)
+        }
+        
+        return persons
+    }
 }
